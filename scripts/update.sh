91fa6bc2e798654c04ec55137418443e265e9870
bolasblack <bolasblack@gmail.com>
2013-09-04 13:35:03 +0800

    feat(scripts/update): 更新了更新脚本

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-if git --git-dir=$HOME/.vim/.git pull --rebase origin master; then
+cd ~/.vim
+git fetch && git checkout origin/master
+
+if [ $? -eq 0 ]; then
   printf '\033[0;34m%s\033[0m\n' 'Vim config has been updated and/or is at the current version.'
 else
   printf '\033[0;31m%s\033[0m\n' 'There was an error updating. Try again later?'
